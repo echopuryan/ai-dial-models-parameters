2b7453ce495d67e6278878f3f4b51a1ab07e6de7
Edward Chopuryan <Edward_Chopuryan@epam.com>
2026-01-12 18:11:39 -0500

    task 2 is done

--- task/2-task-n.py
+++ task/2-task-n.py
@@ -11,9 +11,9 @@ from task.app.main import run
 # - gemini-2.5-pro
 
 run(
-    # TODO:
-    #  1. Provide `deployment_name` with model from the list above👆
-    #  2. Use `n` parameter with value in range from 1 to 5!
+    deployment_name='gpt-4o',
+    print_request=False, # Switch to False if you do not want to see the request in console
+    n=1,
 )
 
 # Pay attention to the number of choices in the response!
